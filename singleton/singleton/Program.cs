e886d5bd8a37e7e18c4d49d0fc4fd66fd31ecd53
KillReal <71165685+jubkka@users.noreply.github.com>
2023-05-31 10:21:10 +0500

    singleton

---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    internal class Program
+    {
+        class QuestItem
+        {
+            internal static QuestItem instance;
+
+            internal string name;
+            internal string description;
+            internal int idQuestItem;
+
+            internal QuestItem(string name, string description, int idQuestItem) 
+            {
+                this.name = name;
+                this.description = description;
+                this.idQuestItem = idQuestItem;
+            }
+
+            public static QuestItem getInstance(string name, string description, int idQuestItem)
+            {
+                if (instance == null)
+                    instance = new QuestItem(name, description, idQuestItem);
+                return instance;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            QuestItem ring = QuestItem.getInstance("Excalibur", "Cool sword", 0);
+
+            Console.WriteLine(ring.name);
+            Console.WriteLine(ring.description);
+            Console.WriteLine(ring.idQuestItem);
+            Console.ReadLine();
+        }
+    }
+}
