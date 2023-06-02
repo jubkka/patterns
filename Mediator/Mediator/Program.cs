d18214474e492af1182e24921ad8ae16c153cc4c
KillReal <71165685+jubkka@users.noreply.github.com>
2023-06-02 08:39:19 +0500

    Add files via upload

---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        abstract class Mediator
+        {
+            public abstract void Send(string msg, Person colleague);
+        }
+
+        abstract class Person
+        {
+            protected Mediator mediator;
+
+            public Person(Mediator mediator) => this.mediator = mediator;
+        }
+
+        class TaxiDriver : Person
+        {
+            public TaxiDriver(Mediator mediator) : base(mediator) { }
+
+            public void Send(string message) => mediator.Send(message, this);
+
+            public void Notify(string message) => Console.WriteLine("Клиент отправил вам сообщение: " + $"{message}");
+        }
+
+        class Client : Person
+        {
+            public Client(Mediator mediator) : base(mediator) { }
+
+            public void Send(string message) => mediator.Send(message, this);
+
+            public void Notify(string message) => Console.WriteLine("Водитель отправил вам сообщеение: " + $"{message}");
+        }
+
+        class Dispatcher : Mediator
+        {
+            public TaxiDriver Taxist { get; set; }
+            public Client Client { get; set; }
+            public override void Send(string msg, Person person)
+            {
+                if (Taxist == person) Client.Notify(msg);
+                else Taxist.Notify(msg);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dispatcher dispatcher = new Dispatcher();
+
+            TaxiDriver taxist = new TaxiDriver(dispatcher);
+            Client client = new Client(dispatcher);
+
+            dispatcher.Taxist = taxist;
+            dispatcher.Client = client;
+
+            taxist.Send("Ало, я на месте, где вы");
+            client.Send("Выходим");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Visitor/Visitor/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    internal class Program
+    {
+        interface Person
+        {
+            void VisitClerking(Clearking clearking);
+            void VisitDevelopmentRoom(DevelopmentRoom developmentRoom);
+            void VisitRestroom(Restroom restroom);
+        }
+
+        class Programmer : Person
+        {
+            public void VisitClerking(Clearking clearking) => Console.WriteLine("Почини компьютер, ты же программист");
+            public void VisitDevelopmentRoom(DevelopmentRoom developmentRoom) => Console.WriteLine("Сиди и пиши программу");
+            public void VisitRestroom(Restroom restroom) => Console.WriteLine("Программист отдыхает");
+        }
+        class Electrician : Person
+        {
+            public void VisitClerking(Clearking clearking) => Console.WriteLine("Почини компьютер, ты же электрик");
+            public void VisitDevelopmentRoom(DevelopmentRoom developmentRoom) => Console.WriteLine("Если лампа не горит, то чините, до тех пор пока не заработает");
+            public void VisitRestroom(Restroom restroom) => Console.WriteLine("Электрик отдыхает");
+        }
+        class Cleaner : Person
+        {
+            public void VisitClerking(Clearking clearking) => Console.WriteLine($"Убирается в {clearking.name}");
+            public void VisitDevelopmentRoom(DevelopmentRoom developmentRoom) => Console.WriteLine($"Убирается в {developmentRoom.name}");
+            public void VisitRestroom(Restroom restroom) => Console.WriteLine($"Убирается в {restroom.name}");
+        }
+
+
+        interface Client 
+        {
+            void Accept(Person person);
+        }
+
+        class Clearking : Client // бухгалтерия
+        {
+            public string name;
+            public Clearking(string name) => this.name = name;
+
+            public void Accept(Person person) => person.VisitClerking(this);
+        }
+        class DevelopmentRoom : Client // отдел разработки
+        {
+            public string name;
+            public DevelopmentRoom(string name) => this.name = name;
+
+            public void Accept(Person person) => person.VisitDevelopmentRoom(this);
+        }
+        class Restroom : Client // комната отдыха
+        {
+            public string name;
+
+            public Restroom(string name) => this.name = name;
+
+            public void Accept(Person person) => person.VisitRestroom(this);
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
