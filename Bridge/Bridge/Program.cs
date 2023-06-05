218554c8e7cd1dd7a709c50fab99a2fa53bcf6ca
KillReal <71165685+jubkka@users.noreply.github.com>
2023-06-05 08:56:02 +0500

    Add files via upload

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    internal class Program
+    {
+        abstract class Color 
+        {
+            internal string color;
+        }
+
+        class Red : Color 
+        {
+            public Red() => color = "Красный";
+        }
+
+        class Blue : Color
+        {
+            public Blue() => color = "Синий";
+        }
+
+        abstract class Figure 
+        {
+            protected string name;
+            protected int countAngle = 0;
+            internal Color color;
+
+            public Figure(Color color) => this.color = color;
+            public Figure() { }
+
+            public void Draw()
+            {
+                if (color == null) { Console.WriteLine($"{name} появилась, но у него отсутствует цвет"); return; }
+
+                Console.WriteLine($"{color.color} {name} нарисован");
+            }
+
+            public void FillColor(Color color)
+            {
+                this.color = color;
+                Console.WriteLine($"Красим {name} в {color.color}");
+            }
+        }
+
+        class Square: Figure 
+        {
+            public Square(Color color) : base(color) 
+            {
+                name = "Квадрат";
+                countAngle = 4;
+            }
+            public Square() => name = "Квадратный";
+        }
+
+        class Circle : Figure
+        {
+            public Circle(Color color) : base(color) => name = "Круг";
+            public Circle() => name = "Круг";
+        }
+
+        static void Main(string[] args)
+        {
+            Figure square = new Square(new Red());
+            Figure circle = new Circle();
+
+            square.Draw();
+            circle.Draw();
+
+            circle.FillColor(new Blue());
+            circle.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Flyweight/Flyweight/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+
+            ParticlesFactory particlesFactory = new ParticlesFactory();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Particle particle = particlesFactory.GetParticle("Fire");
+                if (particle != null)
+                    particle.Spawn(x, y, 100);
+                x += 1;
+                y += 1;
+            }
+
+            Console.Read();
+        }
+
+        abstract class Particle
+        {
+            protected string color;
+            protected string sprite;
+            
+            protected int coords_X;
+            protected int coords_Y;
+            protected int speed;
+            protected string name;
+
+            public virtual void Spawn(int coords_X, int coords_Y, int speed)
+            {
+                this.coords_X = coords_X;
+                this.coords_Y = coords_Y;
+                this.speed = speed;
+
+                Console.WriteLine($"{name} заспавнены в координатах X:{this.coords_X} Y:{this.coords_Y} со скоростью {this.speed}");
+            }
+        }
+
+        class Fire : Particle
+        {
+            public Fire()
+            {
+                color = "Оранжевый";
+                sprite = "*Представим что здесь спрайт огня*";
+                name = "Частицы огня";
+            }
+        }
+        class Water : Particle
+        {
+            public Water()
+            {
+                color = "Голубой";
+                sprite = "*Представим что здесь спрайт воды*";
+                name = "Частицы воды";
+            }
+        }
+
+        class ParticlesFactory
+        {
+            Dictionary<string, Particle> particls = new Dictionary<string, Particle>();
+
+            public ParticlesFactory()
+            {
+                particls.Add("Fire", new Fire());
+                particls.Add("Water", new Water());
+            }
+
+            public Particle GetParticle(string key)
+            {
+                if (particls.ContainsKey(key))
+                    return particls[key];
+                else
+                    return null;
+            }
+        }
+    }
+}
